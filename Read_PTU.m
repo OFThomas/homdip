d4c2108f08847da8ec44d3029391f166ae17b70f
Oli <ot17409@bristol.ac.uk>
2017-11-21 13:07:16 +0000

    Added Lana's matlab codes for coincidence counter and read ptu -> txt

---
+++ Read_PTU.m
@@ -0,0 +1,20 @@
+% Open file and read in data
+fileID = fopen('test3_000.out');    %Change this file name
+Data = fscanf(fileID, '%d %f', [2 Inf]);
+fclose(fileID);
+
+Channel = Data(1,:);    %Arrival channel
+TimeOfArrival = Data(2,:);  %Arrival time (in picoseconds)
+NoOfPhotons = size(Data,2); %Total number of records
+
+CoincidenceCount = 0;
+
+for n = 1:NoOfPhotons-1
+    if Channel(n) ~= Channel(n+1)   %Arrival channel different
+        if TimeOfArrival(n+1) - TimeOfArrival(n) <= 10000   %10ns apart
+            CoincidenceCount = CoincidenceCount + 1;
+        end
+    end
+end
+
+CoincidenceCount
