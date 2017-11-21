54cc26baa7bf0cb595117710e7546892ca9da78f
Oli <ot17409@bristol.ac.uk>
2017-11-21 14:06:31 +0000

    changed to read time in nanoseconds for window and changed type to float

--- coincidencecount.py
+++ coincidencecount.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 datafile=raw_input('enter data file name: ') # read in data file
-coincidencewindow=raw_input('Enter coincidence window: ') #read coinc window
+coincidencewindow=float(raw_input('Enter coincidence window (ns): ')) #read coinc window
 
 #hardcoded for testing
 #datafile='inverter60s_000.out'
 #coincidencewindow=10000000
+
+#converts nanoseconds to picoseconds
+coincidencewindow=coincidencewindow*1000
 
 channel, time = np.genfromtxt(datafile, unpack=True) #split data into two columns
 
