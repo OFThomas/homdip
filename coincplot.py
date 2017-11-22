7323c6286910580cce2d95083184766db67d16c2
Oli <ot17409@bristol.ac.uk>
2017-11-22 14:09:44 +0000

    compared counting 0-1 and 1-0 events (picoharp only counts 0-1) one time difference was doubled 2 counts from 1 but no others increased. added a lot more coincidence count time differences to the plots for 1 microsecond.

--- coincplot.py
+++ coincplot.py
@@ -7,18 +7,24 @@ import matplotlib.pyplot as plt
 #hardcoded for testing
 datafile='apd800mv300s.out'
 bins=100
-delay=[]
 rangeratio=1
+window=10**(-6)
+delay=[]
 
 #split data into two columns
 channel, time = np.genfromtxt(datafile, unpack=True)
 
 for i in range(1, len(channel)-1):
-	if channel[i] == 0 and  channel[i+1]==1:
+	#look for 0 then 1
+	if (channel[i] == 0 and  channel[i+1]==1): #or (channel[i]==1 and channel[i+1]==0):
+		#covert from picos to seconds
  		 delay.append((time[i+1] - time[i])/10**12)
 
+#max range of histogram to plot
 maxrange=rangeratio*max(delay)
+maxrange=window
 
+#plot
 plt.hist(delay,bins=bins, range=(0,maxrange))
 
 plt.ylabel('Counts')
@@ -29,5 +35,11 @@ plt.title('Time between counts ')
 d_name = datafile + str(bins) +'bin'+'.png'
 plt.savefig(d_name, format='png')
 plt.clf()
+
+#write out delay if order of nano seconds
+for i in range(1,len(delay)):
+	if delay[i] <= window:
+		print i, delay[i]
+
 
 
