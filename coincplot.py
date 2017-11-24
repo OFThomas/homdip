c4b6dd229377e5a48dfb15ad0d2f3217c9003474
Oli <ot17409@bristol.ac.uk>
2017-11-24 09:54:55 +0000

    changed time window input to be in nanoseconds so nstime=10 is a 10ns time window

--- coincplot.py
+++ coincplot.py
@@ -6,9 +6,12 @@ import matplotlib.pyplot as plt
 
 #hardcoded for testing
 datafile='apd800mv300s.out'
+#time window
+nstime=10
+#no. of bins
 bins=100
-rangeratio=1
-window=10**(-6)
+
+window=nstime*10**(-9)
 delay=[]
 
 #split data into two columns
@@ -16,12 +19,11 @@ channel, time = np.genfromtxt(datafile, unpack=True)
 
 for i in range(1, len(channel)-1):
 	#look for 0 then 1
-	if (channel[i] == 0 and  channel[i+1]==1): #or (channel[i]==1 and channel[i+1]==0):
+	if (channel[i] == 0 and  channel[i+1]==1) or (channel[i]==1 and channel[i+1]==0):
 		#covert from picos to seconds
  		 delay.append((time[i+1] - time[i])/10**12)
 
 #max range of histogram to plot
-maxrange=rangeratio*max(delay)
 maxrange=window
 
 #plot
