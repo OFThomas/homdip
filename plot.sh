7c672f977bd0499e20c7624bb8bdcf02a2e1656d
john <johnrscott0@gmail.com>
2017-11-24 15:39:09 +0000

    Fixed merge

--- plot.sh
+++ plot.sh
@@ -5,12 +5,6 @@ echo $file
 python coincplot.py << EOF
 $file
 EOF
-<<<<<<< HEAD
 
 #display $file.png &
-done
-=======
- 
-#display $file.png &
 done 
->>>>>>> c463c709825b4a819e47759f1f0927582770d725
