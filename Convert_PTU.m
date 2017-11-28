44010808099b30e90e564478bac51296d4f6ec8d
lana-m <33867817+lana-m@users.noreply.github.com>
2017-11-28 13:18:03 +0000

    Fixed Convert_PTU so you can click on files to convert

--- Convert_PTU.m
+++ Convert_PTU.m
@@ -1,6 +1,7 @@
-function Convert_PTU(File, NoOfFiles)
-% Input: File - Name of file as a string (without number and ptu at end)
-%        NoOfFiles - Number of files to convert
+File = uigetfile('*.ptu');  %Select final file to convert
+File = File(1:end-4);   %Deletes '.ptu'
+NoOfFiles = str2num(File(end));   %Reads in number of files to convert  
+File = File(1:end-1);
 
 for n = 1:NoOfFiles
     FileToConvert = strcat(File,int2str(n),'.ptu');
