e669b57989f5f3d61c4a62d2c41e872391ceff43
lana-m <33867817+lana-m@users.noreply.github.com>
2017-12-01 13:56:20 +0000

    Edited CoincidenceCounter.m to give nicer plot

--- CoincidenceCounter.m
+++ CoincidenceCounter.m
@@ -2,16 +2,16 @@
 filename = uigetfile('*.out');
 Data = importdata(filename)';
 
-Channel = Data(1,:);    %Arrival channel
-TimeOfArrival = Data(2,:);  %Arrival time (in picoseconds)
-NoOfPhotons = size(Data,2); %Total number of records
+Channel = Data(1,:);    % Arrival channel
+TimeOfArrival = Data(2,:);    % Arrival time (in picoseconds)
+NoOfPhotons = size(Data,2);   % Total number of records
 Delay = [];
 
 CoincidenceCount = 0;
 TimeWindow = 5;
 
 for n = 1:NoOfPhotons-1
-    if Channel(n) ~= Channel(n+1)   %Arrival channel different
+    if Channel(n) ~= Channel(n+1)   % Arrival channel different
         if TimeOfArrival(n+1) - TimeOfArrival(n) <= TimeWindow*1000  
             CoincidenceCount = CoincidenceCount + 1;
             Delay = [Delay, TimeOfArrival(n+1) - TimeOfArrival(n)];
@@ -19,22 +19,25 @@ for n = 1:NoOfPhotons-1
     end
 end
 
-CoincidenceCount
-Delay = Delay/1000; % Delay is given in ns
+Delay = Delay/1000; % Delay between photons is given in ns
 
-[y, x] = hist(Delay, 100);
-histogram(Delay, 100);  % Plot histogram
+BinWidth = 0.05;  % 100 bins for every 5ns
+NoOfBins = TimeWindow/BinWidth;
 
-gaussEqn = 'a*exp(-((x-b)/c)^2)+d';  % Fit gaussian
+% Fit gaussian to the histogram
+[y, x] = hist(Delay, NoOfBins);
+gaussEqn = 'a*exp(-((x-b)/c)^2/2)+d';  
 StartPts = [350 1.5 0.5 10];  
 f = fit(x', y', gaussEqn, 'Start', StartPts)
 
+% Plot histogram and fitted Gaussian
+histogram(Delay, NoOfBins);  
 hold on;
 p = plot(f,x,y);
-set(p, 'MarkerSize',0.1);
+set(p, 'MarkerSize',0.01);
 set(p, 'LineWidth',1.5);
 legend('off');
-title(['Time distribution of ', num2str(CoincidenceCount),' coincidences']);
+title([filename, ' Time distribution of ', num2str(CoincidenceCount),' coincidences'], 'Interpreter','none');
 xlabel('Time (ns)');
 ylabel('Counts');
 
