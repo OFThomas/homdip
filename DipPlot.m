6a2b363e88521f909c3d635c4d809b81c8a2b0e8
lana-m <33867817+lana-m@users.noreply.github.com>
2017-12-13 11:10:23 +0000

    Added out files for first dip run 17/12

--- DipPlot.m
+++ DipPlot.m
@@ -4,11 +4,11 @@
 %DipData;
 
 % Distance in um and centred on the dip
-Distum = (Dist-median(Dist))/100;
+Distum = (Dist-median(Dist))*100;
 
 % Fit combination of sinc and gaussian
 fitEqn = 'a*(1 - sinc(b*(x-c))*exp(-((x-c)/d)^2)) + e';
-StartPts = [max(Coincidences) 0.01 0 10000 min(Coincidences)];
+StartPts = [max(Coincidences) 0.01 0 100 min(Coincidences)];
 
 % Plot graph
 f = fit(Distum', Coincidences', fitEqn, 'Start', StartPts);
