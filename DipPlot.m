b7114818933693d23fdbbf1f9a5e0bc7a054c4ba
lana-m <33867817+lana-m@users.noreply.github.com>
2017-12-14 13:54:42 +0000

    Added out files and data for no filter

--- DipPlot.m
+++ DipPlot.m
@@ -12,6 +12,8 @@ Distum = (Dist-median(Dist))*1000;
 % Fit combination of sinc and gaussian
 fitEqn = '(a1*x + a2)*(1 - sinc(b*(x-c))*exp(-((x-c)/d)^2)) + e';
 StartPts = [0.1 max(Coincidences) 0.01 0 100 min(Coincidences)];
+%fitEqn = 'a*(1 - exp(-((x-c)/d)^2)) + e';
+%StartPts = [max(Coincidences) 0 0.5 min(Coincidences)];
 
 % Plot graph
 f = fit(Distum', Coincidences', fitEqn, 'Start', StartPts);
@@ -21,6 +23,7 @@ hold on;
 plot(Distum, Accidentals,'.c','MarkerSize',10);
 set(p, 'LineWidth', 1.2);
 set(p, 'MarkerSize', 10);
+%plot(Distum, Coincidences, '-b');
 xlim([min(Distum) max(Distum)]);
 %xlim([-70 70]);
 ylim([0 max(Coincidences+1000)]);
